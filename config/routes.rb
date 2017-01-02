c47b974791ed8461cfa7a1fd45785ebee4492038
hparekh <hparekh@nd.edu>
2017-01-02 15:47:58 -0500

    setup for accessing multiple views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/homepage'
+  #get 'welcome/homepage'
+  match ':controller(/:action)', :via => :get
   root 'welcome#login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
